ae51ef93eb92d7ab9fc704dacf43bd9a5fc4bdd7
Muhammad Adnan <adnan108123796@gmail.com>
2022-06-17 16:53:04 +0500

    log sign

---
+++ lib/auth/splash.dart
@@ -0,0 +1,47 @@
+// import 'dart:async';
+
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:ecommerce_app/allcolors.dart';
+import 'package:ecommerce_app/auth/register.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   Timer(
+  //     Duration(seconds: 1),
+  //     () => Navigator.push(
+  //       context,
+  //       MaterialPageRoute(
+  //         builder: (context) => Register(),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    
+      body: AnimatedSplashScreen(
+        backgroundColor: AllColors.primarycolor,
+        splashIconSize: 200,
+        duration: 600,
+        splashTransition: SplashTransition.fadeTransition,
+        splash: "images/image_logo.png",
+        nextScreen: Register(),
+        pageTransitionType: PageTransitionType.fade,
+      ),
+    );
+  }
+}
